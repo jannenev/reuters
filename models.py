df41a4a073a057455fbdb0915756084ebc0315a5
jannenev <11726563+jannenev@users.noreply.github.com>
2018-12-14 12:57:42 +0200

    moved couple models to models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ import torch as torch
 
 
 # Conv1d - Maxpool or Avgpool
-class CNNPool5(nn.Module):
+class CNNPool(nn.Module):
     def __init__(self, vocab_size, embedding_dim, nr_filters, filter_size, output_dim, pool='max'):
         super().__init__()
         
@@ -76,15 +76,14 @@ class CNNPool5(nn.Module):
         x = self.dropout(x)        
         x = self.fc(x)
 
-        #x = F.sigmoid(x)
+        x = F.sigmoid(x)
         return(x)
-
 
 
 
 # 1D conv MAX + AVG Pool concatenated
 class CNN(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, nr_filters, filter_size, output_dim):
+    def __init__(self, vocab_size, embedding_dim, nr_filters, filter_size, output_dim, dropout):
         super().__init__()
         
         # nn.Embedding(num_embeddings, : size of the dictionary of embeddings
@@ -100,7 +99,7 @@ class CNN(nn.Module):
         #self.conv2 = nn.Conv1d(in_channels= nr_filters, out_channels=nr_filters*2, kernel_size=filter_size )
         #self.fc = nn.Linear(in_features= nr_filters*2, out_features= output_dim)
         
-        self.dropout = nn.Dropout(0.5) 
+        self.dropout = nn.Dropout(dropout) 
         
         self.nr_filters = nr_filters
         self.printmore= torch.zeros(1)
@@ -152,7 +151,7 @@ class CNN(nn.Module):
         # max + avg pool and concat
         # This will features 2* long, so next layer needs to input 2*len
         p1 = F.max_pool1d(x, x.size(2)).squeeze(2)          #print(x.shape) # [64, 128, 1]
-        p2 = F.max_pool1d(x, x.size(2)).squeeze(2)          #print(x.shape) # [64, 128, 1]
+        p2 = F.avg_pool1d(x, x.size(2)).squeeze(2)          #print(x.shape) # [64, 128, 1]
         x = torch.cat((p1,p2), 1)
         
         
@@ -165,9 +164,9 @@ class CNN(nn.Module):
         
         x = self.fc(x)
 
-        #x = F.sigmoid(x)
+        x = (x)
 
-        return(x)
+        return F.sigmoid(x)
 
 
 # In[ ]:
@@ -190,13 +189,9 @@ class CNN(nn.Module):
 #criterion = criterion.to(device)
 
 
-# ### 2D conv model, with multi size kernels 
-
-# In[200]:
-
-
+# 2D conv model, with multi size kernels 
 class CNN2(nn.Module):
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, dropout):
+    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, dropout=0.5):
         super().__init__()
         
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
@@ -205,74 +200,40 @@ class CNN2(nn.Module):
         self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
-        
         #x = [sent len, batch size]
         
-        x = x.permute(1, 0)
-                
+        x = x.permute(1, 0)        
         #x = [batch size, sent len]
         
         embedded = self.embedding(x)
-                
         #embedded = [batch size, sent len, emb dim]
         
-        embedded = embedded.unsqueeze(1)
-        
+        embedded = embedded.unsqueeze(1)        
         #embedded = [batch size, 1, sent len, emb dim]
         
-        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
-            
+        conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]            
         #conv_n = [batch size, n_filters, sent len - filter_sizes[n]]
         
-        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
-        
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]        
         #pooled_n = [batch size, n_filters]
         
         cat = self.dropout(torch.cat(pooled, dim=1))
-
         #cat = [batch size, n_filters * len(filter_sizes)]
             
-        return self.fc(cat)
+        return F.sigmoid(self.fc(cat))
 
-
-# In[201]:
-
-
-#INPUT_DIM = len(TEXT.vocab)
-#EMBEDDING_DIM = 50
+    
 #N_FILTERS = 100
 #FILTER_SIZES = [3,4,5]
-#OUTPUT_DIM = 126
-#DROPOUT = 0.5
-
-#model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT)
-#model.embedding.weight.data.copy_(pretrained_embeddings)
-#model = model.to(device)
-
-#optimizer = optim.Adam(model.parameters())
-#criterion = nn.BCEWithLogitsLoss()
-#criterion = criterion.to(device)
+#model = CNN2(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT)
 
 
-# In[29]:
-
-
-##INPUT_DIM = len(TEXT.vocab)
-#EMBEDDING_DIM = 50
 #N_FILTERS = 200
 #FILTER_SIZES = [3,5,7]
-#OUTPUT_DIM = 126
-#DROPOUT = 0.5
-
 #model = CNN(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT)
-#model.embedding.weight.data.copy_(pretrained_embeddings)
-#model = model.to(device)
-
 #optimizer = optim.Adam(model.parameters())
-#criterion = nn.BCEWithLogitsLoss()
-#criterion = criterion.to(device)
-
 # 14 epochs gave f1 0.84
+
 
 
 # Simple model
@@ -310,15 +271,9 @@ class RNNSimple(nn.Module):
 # # LSTM model
 
 # model
-#INPUT_DIM = len(TEXT.vocab)
-#EMBEDDING_DIM = 50
 #HIDDEN_DIM = 250
-#OUTPUT_DIM = 126
 #N_EPOCHS = 50
-
-
 #N_LAYERS = 1
-#BIDIRECTIONAL = False
 
 class LSTM(nn.Module):
     def __init__(self, input_dim, output_dim, embedding_dim, hidden_dim,
@@ -333,15 +288,13 @@ class LSTM(nn.Module):
         self.hidden_dim = hidden_dim
         self.device=device
         
-        
-        
     def forward(self, x):
         x = self.embedding(x)
         x, (h, c) = self.lstm(x, self.hidden)
         x = F.relu(self.fc1(x[-1]))
         x = F.dropout(x)
-        output = self.fc2(x)
-        return output
+        x = self.fc2(x)
+        return F.sigmoid(x)
     
     def init_hidden(self, batch_size):
         if self.bidirectional:
@@ -357,8 +310,78 @@ class LSTM(nn.Module):
 #model.embedding.weight.data.copy_(TEXT.vocab.vectors)
 #optimizer = optim.SGD(model.parameters(), lr=0.02)
 ## optimizer = optim.Adam(model.parameters(), lr=0.01)
-#criterion = nn.BCEWithLogitsLoss()
 #model = model.to(device)
 #criterion = criterion.to(device)
 #model.hidden = model.init_hidden()
 
+
+
+# LSTM Ver 2 - converges with 24k examples
+
+class LSTMv2(nn.Module):
+    def __init__(self, input_dim, output_dim, embedding_dim, hidden_dim,
+                n_layers, bidirectional, device):
+        super().__init__()
+        self.n_layers = n_layers
+        self.bidirectional=bidirectional
+        self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=n_layers, bidirectional=bidirectional) # dropout=0.5)
+        #self.fc1 = nn.Linear(2*hidden_dim, output_dim) if bidirectional else nn.Linear(hidden_dim, output_dim)
+        self.fc1 = nn.Linear(2*hidden_dim, 200) if bidirectional else nn.Linear(hidden_dim, 200)
+        self.fc2 = nn.Linear(200, output_dim)
+        self.hidden_dim = hidden_dim
+        self.device=device
+        
+    def forward(self, x):
+        x = self.embedding(x)
+        x, (h, c) = self.lstm(x, self.hidden)
+        
+        x = self.fc1(x[-1])
+        x = F.relu(x)
+        x = F.dropout(x)
+        x = self.fc2(x)
+        x = F.sigmoid(x)
+        return x
+    
+    def init_hidden(self, batch_size):
+        if self.bidirectional:
+            return (torch.zeros(2*self.n_layers, batch_size, self.hidden_dim).to(self.device),
+                  torch.zeros(2*self.n_layers, batch_size, self.hidden_dim).to(self.device))
+        else:
+            return (torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device),
+                  torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device))
+        
+
+# LSTM ver3 batchnorm
+class LSTMv3(nn.Module):
+    def __init__(self, input_dim, output_dim, embedding_dim, hidden_dim,
+                n_layers, bidirectional, device):
+        super(LSTM, self).__init__()
+        self.n_layers = n_layers
+        self.bidirectional=bidirectional
+        self.embedding = nn.Embedding(input_dim, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=n_layers, bidirectional=bidirectional) # dropout=0.5)
+        self.fc1 = nn.Linear(2*hidden_dim, 200) if bidirectional else nn.Linear(hidden_dim, 200)
+        self.bn1 = nn.BatchNorm1d(hidden_dim)
+        
+        self.fc2 = nn.Linear(200, output_dim)
+        self.bn2 = nn.BatchNorm1d(200)
+        self.hidden_dim = hidden_dim
+        self.device=device
+        self.dropout = nn.Dropout(0.5)
+        
+    def forward(self, x):
+        x = self.embedding(x)
+        x, lstm_h = self.lstm(x, self.hidden)
+        x = F.relu(self.fc1(x[-1]))
+        x = self.dropout(x)
+        output = self.fc2(self.bn2(x))
+        return F.sigmoid(output)
+    
+    def init_hidden(self, batch_size):
+        if self.bidirectional:
+            return (torch.zeros(2*self.n_layers, batch_size, self.hidden_dim).to(self.device),
+                  torch.zeros(2*self.n_layers, batch_size, self.hidden_dim).to(self.device))
+        else:
+            return (torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device),
+                  torch.zeros(self.n_layers, batch_size, self.hidden_dim).to(self.device))
